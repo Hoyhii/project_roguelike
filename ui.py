4e9acd362103ea9c89a1570bdf220cf441ef634c
Hoyhii <Hoyhitv@gmail.com>
2021-05-31 01:47:40 +0200

    changed map size,fix display board function

--- ui.py
+++ ui.py
@@ -1,6 +1,5 @@
 def display_board(board):
     for elem in board:
-        print('\n')
         for i in elem:
-            print (i,end='')
-    pass
+            print(i, end='')
+        print('\n',end='')
