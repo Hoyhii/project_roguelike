0a71d6fed5c97375aeebce9a252a7c6d90d61161
Hoyhii <Hoyhitv@gmail.com>
2021-05-31 01:20:51 +0200

    Implement put player on board function and display board function

--- ui.py
+++ ui.py
@@ -1,8 +1,6 @@
 def display_board(board):
-    '''
-    Displays complete game board on the screen
-
-    Returns:
-    Nothing
-    '''
+    for elem in board:
+        print('\n')
+        for i in elem:
+            print (i,end='')
     pass
