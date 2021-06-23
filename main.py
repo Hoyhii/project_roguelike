1310a4b3b1fc018f8efcfe43d964010703940a13
Hoyhii <Hoyhitv@gmail.com>
2021-06-23 19:06:23 +0200

    implement collision detection and proper player position refresh

--- main.py
+++ main.py
@@ -21,15 +21,36 @@ def create_player(PLAYER_START_X,PLAYER_START_Y):
     }
     return player
 
+def no_collision(board,coordinate_y,coordinate_x):
+    if board[coordinate_y][coordinate_x] == '#':
+            return False
+    return True
 
+def valid_move(player,key,board):
+    coordinate_x = player['x']
+    coordinate_y = player['y']
+    if key == 'w':
+        coordinate_y -= 1
+        return no_collision(board,coordinate_y,coordinate_x)
+    if key == 'a':
+        coordinate_x -= 1
+        return no_collision(board,coordinate_y,coordinate_x)
+    if key == 's':
+        coordinate_y += 1
+        return no_collision(board,coordinate_y,coordinate_x)
+    if key == 'd':
+        coordinate_x += 1
+        return no_collision(board,coordinate_y,coordinate_x)
+    
 def main():
     player = create_player(PLAYER_START_X,PLAYER_START_Y)
     board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
     
 
-    util.clear_screen()
+    
     is_running = True
     while is_running:
+        util.clear_screen()
         board = engine.put_player_on_board(board, player)
         ui.display_board(board)
 
@@ -37,17 +58,29 @@ def main():
         if key == 'q':
                 is_running = False
         if key in ('w','a','s','d'):
-            
+            previous_x = player['x']
+            previous_y = player['y']
             if key == 'w':
-                player['y'] -= 1
+                if valid_move(player,key,board):
+                    player['y'] -= 1
+                else:
+                    continue
             elif key == 'a':
-                player['x'] -= 1
+                if valid_move(player,key,board):
+                    player['x'] -= 1
+                else:
+                    continue
             elif key == 's':
-                player['y'] += 1
+                if valid_move(player,key,board):
+                    player['y'] += 1
+                else:
+                    continue
             elif key == 'd':
-                player['x'] += 1
-            
-            
+                if valid_move(player,key,board):
+                    player['x'] += 1
+                else:
+                    continue
+            board[previous_y][previous_x] = " "
             
 
             
