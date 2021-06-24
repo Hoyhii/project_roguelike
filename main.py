1cdeabf8f27740907f535c2dbcf1366f234cfc82
Hoyhii <Hoyhitv@gmail.com>
2021-06-24 23:16:11 +0200

    implement sword item pick up, adding to inventory

--- main.py
+++ main.py
@@ -21,27 +21,37 @@ def create_player(PLAYER_START_X,PLAYER_START_Y):
     }
     return player
 
+
 def no_collision(board,coordinate_y,coordinate_x):
     if board[coordinate_y][coordinate_x] == '#':
             return False
     return True
 
-def valid_move(player,key,board):
+
+def check_move(player,key,board):
     coordinate_x = player['x']
     coordinate_y = player['y']
     if key == 'w':
         coordinate_y -= 1
-        return no_collision(board,coordinate_y,coordinate_x)
+        return (no_collision(board,coordinate_y,coordinate_x), check_if_item(board, coordinate_y, coordinate_x, player))
     if key == 'a':
         coordinate_x -= 1
-        return no_collision(board,coordinate_y,coordinate_x)
+        return (no_collision(board,coordinate_y,coordinate_x), check_if_item(board, coordinate_y, coordinate_x, player))
     if key == 's':
         coordinate_y += 1
-        return no_collision(board,coordinate_y,coordinate_x)
+        return (no_collision(board,coordinate_y,coordinate_x), check_if_item(board, coordinate_y, coordinate_x, player))
     if key == 'd':
         coordinate_x += 1
-        return no_collision(board,coordinate_y,coordinate_x)
+        return (no_collision(board,coordinate_y,coordinate_x), check_if_item(board, coordinate_y, coordinate_x, player))
     
+
+def check_if_item(board, coordinate_y, coordinate_x, player):
+    if board[coordinate_y][coordinate_x] == 'S':
+        player['items']['sword'] = {'type':'sword','damage':3}
+        print(player['items'])
+    return player
+
+
 def main():
     player = create_player(PLAYER_START_X,PLAYER_START_Y)
     board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
@@ -61,22 +71,26 @@ def main():
             previous_x = player['x']
             previous_y = player['y']
             if key == 'w':
-                if valid_move(player,key,board):
+                valid_move, player = check_move(player,key,board)
+                if valid_move:
                     player['y'] -= 1
                 else:
                     continue
             elif key == 'a':
-                if valid_move(player,key,board):
+                valid_move, player = check_move(player,key,board)
+                if valid_move:
                     player['x'] -= 1
                 else:
                     continue
             elif key == 's':
-                if valid_move(player,key,board):
+                valid_move, player = check_move(player,key,board)
+                if valid_move:
                     player['y'] += 1
                 else:
                     continue
             elif key == 'd':
-                if valid_move(player,key,board):
+                valid_move, player = check_move(player,key,board)
+                if valid_move:
                     player['x'] += 1
                 else:
                     continue
