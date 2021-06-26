74b13a07f7521a05fc7884f8b8abdd03c629c50e
Hoyhii <Hoyhitv@gmail.com>
2021-06-26 02:27:18 +0200

    implement item generation and item placement on the board

--- engine.py
+++ engine.py
@@ -22,12 +22,13 @@ def create_board(width, height):
         if board[row][col] == '#':
             board[row][col] = 'G'
             break
-    for i in range(3):
+    for i in range(random.randrange(3,6)):
         while True:
             row = random.randrange(0,height)
             col = random.randrange(0,width)
             if board[row][col] == ' ':
-                board[row][col] = 'S'
+                item = get_all_items()[random.randrange(0,len(get_all_items()))]
+                board[row][col] = item['icon']
                 break
     return board
 
@@ -39,3 +40,23 @@ def put_player_on_board(board, player):
     board[y][x] = player['icon']
 
     return board
+
+def create_item(type):
+    item = {}
+    if type == 'sword':
+        item = {'type':'sword','additional_dmg':random.randrange(2,6)}
+    if type == 'key':
+        item = {'type':'key'}
+    if type == 'food':
+        item = {'type':'food', 'restore_hp': random.randrange(10,25)}
+    if type == 'armor':
+        item = {'type':'armor', 'additional_armor':random.randrange(10,20)}
+    return item
+
+    
+def get_all_items():
+    items = [{'icon':'S', 'type':'sword'},
+            {'icon':'K', 'type':'key'},
+            {'icon':'F', 'type':'food'},
+            {'icon':'A','type':'armor'}]
+    return items
