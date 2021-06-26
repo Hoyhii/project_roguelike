8fab995bdfdae16ca9aac8824065016e019cab97
Hoyhii <Hoyhitv@gmail.com>
2021-06-26 02:31:39 +0200

    refactoring, add variety of lootable items with stat generation, implement switchable inventory display

--- main.py
+++ main.py
@@ -16,8 +16,12 @@ def create_player(PLAYER_START_X,PLAYER_START_Y):
         'x':PLAYER_START_X,
         'race':1,
         'name':'',
-        'items':{},
-        'icon':'@'
+        'items':[],
+        'icon':'@',
+        'base_dmg':5,
+        'base_hp':100,
+        'current_hp':100,
+        'base_armor':0
     }
     return player
 
@@ -28,27 +32,34 @@ def no_collision(board,coordinate_y,coordinate_x):
     return True
 
 
-def check_move(player,key,board):
+def check_move(player,key_pressed,board):
     coordinate_x = player['x']
     coordinate_y = player['y']
-    if key == 'w':
+    if key_pressed == 'w':
         coordinate_y -= 1
-        return (no_collision(board,coordinate_y,coordinate_x), check_if_item(board, coordinate_y, coordinate_x, player))
-    if key == 'a':
+    elif key_pressed == 'a':
         coordinate_x -= 1
-        return (no_collision(board,coordinate_y,coordinate_x), check_if_item(board, coordinate_y, coordinate_x, player))
-    if key == 's':
+    elif key_pressed == 's':
         coordinate_y += 1
-        return (no_collision(board,coordinate_y,coordinate_x), check_if_item(board, coordinate_y, coordinate_x, player))
-    if key == 'd':
+    elif key_pressed == 'd':
         coordinate_x += 1
-        return (no_collision(board,coordinate_y,coordinate_x), check_if_item(board, coordinate_y, coordinate_x, player))
+    return (no_collision(board,coordinate_y,coordinate_x), check_if_item(board, coordinate_y, coordinate_x, player))
     
 
 def check_if_item(board, coordinate_y, coordinate_x, player):
     if board[coordinate_y][coordinate_x] == 'S':
-        player['items']['sword'] = {'type':'sword','damage':3}
-        print(player['items'])
+        player['items'].append(engine.create_item('sword'))
+    if board[coordinate_y][coordinate_x] == 'A':
+        player['base_armor']+=engine.create_item('armor')['additional_armor']
+        #player['armor'].append(engine.create_item('armor'))
+    if board[coordinate_y][coordinate_x] == 'F':
+        food = engine.create_item('food')
+        if(player['current_hp'] + food['restore_hp'] < player['base_hp']):
+            player['current_hp'] += food['restore_hp']
+        else:
+            player['current_hp'] = player['base_hp']
+    if board[coordinate_y][coordinate_x] == 'K':
+        player['items'].append(engine.create_item('key'))
     return player
 
 
@@ -59,14 +70,22 @@ def main():
 
     
     is_running = True
+    is_inventory_visible = False
     while is_running:
         util.clear_screen()
         board = engine.put_player_on_board(board, player)
-        ui.display_board(board)
+        ui.display_board(board,player)
+        if is_inventory_visible:
+            ui.display_stats(player)
         key = util.key_pressed()
 
         if key == 'q':
                 is_running = False
+        if key == 'i':
+            if is_inventory_visible:
+                is_inventory_visible = False
+            else:
+                is_inventory_visible = True
         if key in ('w','a','s','d'):
             previous_x = player['x']
             previous_y = player['y']
