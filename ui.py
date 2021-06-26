1287a05e1edccbbf143522af66ab7d4b3b6b363b
Hoyhii <Hoyhitv@gmail.com>
2021-06-26 02:34:12 +0200

    implement display_stats feature

--- ui.py
+++ ui.py
@@ -1,5 +1,27 @@
-def display_board(board):
+def display_board(board,player):
     for elem in board:
         for i in elem:
             print(i, end='')
         print('\n',end='')
+
+def display_stats(player):
+    print("hp: " + str(player['current_hp']) + ", armor: " + str(player['base_armor']))
+    for item in player['items']:
+        if(item['type'] == 'sword'):
+
+            #TODO
+
+            print("type: " , item['type'], ", damage:")
+        else:
+            print("type: " , item['type'])
+        
+    """ inventory_x_length = 10
+    inventory_y_length = 5
+    for i in range(inventory_y_length):
+        for j in range(inventory_x_length):
+            if(i == 0 or i == inventory_x_length-1):
+                print("-", end="")
+            else:
+                print("|")
+ """
+    
